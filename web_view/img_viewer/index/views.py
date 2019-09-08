ecdceaa71836b3153efead1fd4a40156d19d4ae2
j129008 <black.heptagram@gmail.com>
2019-09-08 12:54:29 +0800

    :wrench: list static folder

--- web_view/img_viewer/index/views.py
+++ web_view/img_viewer/index/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render_to_response
 from glob import glob
 
 def index(request):
-    folders = glob('../../img/*')
+    folders = glob('static/*')
     return render_to_response('index.html', {
         'folders': [folder.split('/')[-1] for folder in folders]
     })
