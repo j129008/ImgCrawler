d6995d927097f7511b72ebbfff4ca48530289a09
davidchang <davidchang@ovotv.com>
2019-09-08 07:08:46 +0000

    :wrench: update sample

--- itmutu.py
+++ itmutu.py
@@ -1,7 +1,14 @@
 from lib.crawlib.webCraw import Craw
 
-craw = Craw('http://www.itmtu.com/mm/35178/4', next_keyword='下一页', img_pattern='http://img.itmtu.com/.*.jpg')
-folder = 'test'
+base_url = 'http://www.itmtu.com'
+img_type = 'mm'
+img_id = 67496
+
+url = '{}/{}/{}/'.format(base_url, img_type, img_id)
+
+craw = Craw(url, next_keyword='下一页', img_pattern='http://img.itmtu.com/.*.jpg')
+craw.setUrlPattern(url + '.*')
+folder = img_id
 path = 'web_view/img_viewer/static/{}/'.format(folder)
 imgs = craw.getAllImg(path)
 
