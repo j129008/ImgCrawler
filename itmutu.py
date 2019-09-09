f23013736a2fcc00d82d15956dd1caabe2998af5
davidchang <davidchang@ovotv.com>
2019-09-09 15:19:03 +0000

    :wrench: download to media folder

--- itmutu.py
+++ itmutu.py
@@ -10,6 +10,6 @@ url = '{}/{}/{}/'.format(base_url, img_type, img_id)
 craw = Craw(url, next_keyword='下一页', img_pattern='http://img.itmtu.com/.*.jpg')
 craw.setUrlPattern(url + '.*')
 folder = img_id
-path = 'web_view/img_viewer/static/{}/'.format(folder)
+path = 'web_view/img_viewer/media/{}/'.format(folder)
 imgs = craw.getAllImg(path)
 
