0853c7bf29d8ccc284f30f35d53418c006bc680d
davidchang <davidchang@ovotv.com>
2019-09-10 17:01:20 +0000

    :sparkles: use GET download img

--- web_view/img_viewer/index/views.py
+++ web_view/img_viewer/index/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render_to_response
 from glob import glob
+from .lib.crawlib.webCraw import Craw
+import sys
+import threading
+
+
+def fetchImg(img_id):
+    base_url = 'http://www.itmtu.com'
+    img_type = 'mm'
+
+    url = '{}/{}/{}/'.format(base_url, img_type, img_id)
+
+    craw = Craw(url, next_keyword='下一页', img_pattern='http://img.itmtu.com/.*.(jpg|png)')
+    craw.setUrlPattern(url + '.*')
+    folder = img_id
+    path = '/root/ImgCrawler/web_view/img_viewer/media/{}/'.format(folder)
+    imgs = craw.getAllImg(path)
 
 def index(request):
+    if 'img_id' in request.GET:
+        img_id = request.GET['img_id']
+        print('img id: {}'.format(img_id))
+        thread = threading.Thread(target=fetchImg, args=(img_id, ))
+        thread.start()
     folders = glob('media/*')
     return render_to_response('index.html', {
         'folders': [folder.split('/')[-1] for folder in folders]
